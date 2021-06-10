56689744cfa0a26ce669796df7cfc1c1b1b192b9
Henry <hjs@lumel.co.uk>
2021-06-10 15:38:44 +0100

    Tag can build for dev and latest.

--- tag.sh
+++ tag.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-if [ -z $1 ]
+TAG=$1
+if [ -z $TAG ]
 then
-	echo "Usage: $0 <version>" 1>&2
-	echo "    e.g. $0 6.0.41" 1>&2
-	exit 1
+	CURRENT_BRANCH=$(git status | grep 'On branch' | cut -f3 -d\  ) 
+	if [ $CURRENT_BRANCH == "dev" ]
+	then
+		TAG="dev"
+	else
+		TAG=latest
+	fi
 fi
 
-git tag -a $1 -m $1
-git push --tags
+
+if [ $TAG != latest -q $TAG != dev ]
+then
+	git tag -a $1 -m $1
+	git push --tags
+fi
+
+docker build -t lumel/unifi-controller:$TAG .
+docker push lumel/unifi-controller:$TAG
 
