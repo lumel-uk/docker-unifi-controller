21b5ad8f1d9d012861f46e9253f870f3eaf9dde1
Henry <hjs@lumel.co.uk>
2021-10-02 11:37:26 +0100

    Do it, not just say I'll do it.

--- scripts/tag_dev_latest_version.sh
+++ scripts/tag_dev_latest_version.sh
@@ -23,17 +23,18 @@ else
 	echo "Unsupported tag $TAG" 1>&2
 	exit 0
 fi
-#hjs@cortado:~/Dev/docker-unifi-controller/scripts$ docker cp $id:/unifi-version -
-#unifi-version0100644000000000000000000000003014126015204011751 0ustar0000000000000000Version: 6.4.54-16067-1
+
 id=$(docker create $IMAGE:$TAG)
 vnstr=$(docker cp $id:/unifi-version -)
 unifi_version=$(cut -f2 -d: <<< $vnstr | cut -f1 -d- | xargs)
 echo Detected Unifi Version $unifi_version
+
 NEW_TAG=$TAG_PREFIX$unifi_version
 echo Retagging as $NEW_TAG
+docker  tag $IMAGE:$TAG $IMAGE:$NEW_TAG
 
-echo docker  tag $IMAGE:$TAG $IMAGE:$NEW_TAG
-echo docker push lumel/unifi-controller:$NEW_TAG
+echo Pushing image
+docker push lumel/unifi-controller:$NEW_TAG
 
 docker rm $id
 
