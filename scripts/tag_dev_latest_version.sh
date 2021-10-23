9e10614320064886398da1643cbd2f24d327e988
Henry <hjs@lumel.co.uk>
2021-10-24 00:02:32 +0100

    Updated docker push in response to new aws managed docker image

--- scripts/tag_dev_latest_version.sh
+++ scripts/tag_dev_latest_version.sh
@@ -34,7 +34,7 @@ echo Retagging as $NEW_TAG
 docker  tag $IMAGE:$TAG $IMAGE:$NEW_TAG
 
 echo Pushing image
-docker push lumel/unifi-controller:$NEW_TAG
+docker push --all-tags lumel/unifi-controller:$NEW_TAG
 
 docker rm $id
 
